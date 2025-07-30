5a10b8e8bce6330b78877e64ad9af02b33b79f68
rastr <romanmilko123@gmail,com>
2025-07-30 16:29:42 +0200

    Settings setup

---
+++ src/app/core/settings.py
@@ -0,0 +1,20 @@
+from pydantic_settings import BaseSettings
+from starlette.config import Config
+
+env_path = ".env"
+config = Config(env_path)
+
+
+class PostgresSettings(BaseSettings):
+    POSTGRES_USER: str = config("POSTGRES_USER", default="postgres")
+    POSTGRES_PASSWORD: str = config("POSTGRES_PASSWORD", default="postgres")
+    POSTGRES_SERVER: str = config("POSTGRES_SERVER", default="localhost")
+    POSTGRES_PORT: int = config("POSTGRES_PORT", default=5432)
+    POSTGRES_DB: str = config("POSTGRES_DB", default="postgres")
+
+    def get_url(self):
+        return (f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
+                f"{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}")
+
+
+settings = PostgresSettings()
