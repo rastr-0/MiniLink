d72abc2ae432a05cb9b8618eba791cb4e28c59bf
rastr <romanmilko123@gmail,com>
2025-07-30 16:30:28 +0200

    Entry position of the project

---
+++ src/app/main.py
@@ -0,0 +1,22 @@
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from src.app.api.v1.convert import router as convert_router
+from src.app.api.v1.auth import router as auth_router
+from src.app.core.db.init_db import init_db
+from src.app.core.logger import setup_logging, LOGGING_CONFIG
+from dotenv import load_dotenv
+
+setup_logging(log_config=LOGGING_CONFIG)
+
+
+@asynccontextmanager
+async def lifespan(_app: FastAPI):
+    # loading .env data
+    load_dotenv()
+    await init_db()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=convert_router)
+app.include_router(router=auth_router)
