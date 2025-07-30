76541a05c08af95c7fcea5c2ca3f39576fd9e680
rastr <romanmilko123@gmail,com>
2025-07-30 16:28:35 +0200

    Logging setup

---
+++ src/app/core/logger.py
@@ -0,0 +1,54 @@
+import logging
+import logging.config
+
+
+def setup_logging(default_level=logging.INFO, log_config: dict = None):
+    if log_config:
+        logging.config.dictConfig(log_config)
+    else:
+        logging.basicConfig(level=default_level)
+
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+            "level": "INFO",
+        },
+        "file": {
+            "class": "logging.FileHandler",
+            "formatter": "standard",
+            "level": "DEBUG",
+            "filename": "app.log"
+        },
+    },
+    "loggers": {
+        "Database": {
+            "handlers": ["console", "file"],
+            "level": "DEBUG",
+            "propagate": False
+        },
+        "Request": {
+            "handlers": ["console", "file"],
+            "level": "DEBUG",
+            "propagate": False
+        },
+        "InternalLogger": {
+            "handlers": ["console", "file"],
+            "level": "DEBUG",
+            "propagate": False
+        }
+    }
+}
+
+db_logger = logging.getLogger("Database")
+requests_logger = logging.getLogger("Request")
+internal_logger = logging.getLogger("InternalLogger")
