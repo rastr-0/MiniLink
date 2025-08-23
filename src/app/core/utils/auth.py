be9459cad1b661cc405a846c965b39018347d2cc
rastr <romanmilko123@gmail,com>
2025-08-23 16:49:54 +0200

    Fixed old logging dependencies to new ones + added comments

--- src/app/core/utils/auth.py
+++ src/app/core/utils/auth.py
@@ -1,30 +1,31 @@
-# annotations
-from typing import Annotated
+"""
+Utility functions for authentication.
 
-from sqlalchemy.exc import SQLAlchemyError
-# SQLAlchemy ORM
-from sqlalchemy.ext.asyncio import AsyncSession
-from src.app.core.db.database import get_db
-# CRUD
-from src.app.crud.operations import get_existing_user
-# fastapi related
+Includes:
+- Creating an access token
+- Authentication of the user
+- Getting currently logged-in user
+
+"""
+
 from fastapi import status, HTTPException
 from fastapi.params import Depends
 from fastapi.security import OAuth2PasswordBearer
-# security
-import jwt
-from jwt import InvalidTokenError
-from src.app.core.utils import verify_password
-# logging
-from src.app.core.logger import internal_logger
-# others
-from os import getenv
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
 from datetime import timedelta, timezone, datetime
-# schemas
+from typing import Annotated
+from os import getenv
+import logging
+import jwt
+
+from src.app.core.db.database import get_db
+from src.app.crud.operations import get_existing_user
 from src.app.schemas import TokenData, UserResponse
-# exceptions
 from src.app.core import exceptions
-from jwt import InvalidKeyError, PyJWTError
+from src.app.core.utils import verify_password
+
+logger = logging.getLogger(__name__)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/v1/token")
 
@@ -35,18 +36,19 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
         # if expiration time is provided
         expires = datetime.now(timezone.utc) + expires_delta
     else:
-        # if not, then default time is 30 minutes
+        # if not, then the default time is 30 minutes
         expires = datetime.now(timezone.utc) + timedelta(minutes=30)
     try:
         to_encode.update({"expiration": str(expires)})
         encoded_jwt = jwt.encode(to_encode, getenv("SECRET_KEY"), getenv("ALGORITHM"))
-        internal_logger.info(f"Access token was successfully formed for user: {data['sub']}")
-    except InvalidKeyError as e:
+    except jwt.InvalidKeyError as e:
         raise exceptions.TokenCreationError("Missing 'sub' in token data") from e
-    except PyJWTError as e:
+    except jwt.PyJWTError as e:
         raise exceptions.TokenCreationError("Failed to create JWT token") from e
     except Exception as e:
         raise exceptions.TokenCreationError("Failed to create JWT token due to unkown error") from e
+
+    logger.info(f"Access token was successfully formed for user: {data['sub']}")
 
     return encoded_jwt
 
@@ -78,7 +80,7 @@ async def get_current_user(
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
-    except InvalidTokenError:
+    except jwt.InvalidTokenError:
         raise credentials_exception
 
     user = await get_existing_user(db, token_data.username)
@@ -86,7 +88,7 @@ async def get_current_user(
     if user is None:
         raise credentials_exception
 
-    internal_logger.info(f"Information about user: {user.username} successfully retrieved")
+    logger.info(f"Information about user: {user.username} successfully retrieved")
 
     return UserResponse(
         id=user.id,
